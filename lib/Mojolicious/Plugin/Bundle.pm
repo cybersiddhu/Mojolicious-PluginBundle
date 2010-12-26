298a54112e9a72b724306db08aeccabf272ea536
Siddhartha Basu <biosidd@gmail.com>
2010-12-26 10:16:53 -0600

    put the file in proper namespace

---
+++ lib/Mojolicious/Plugin/Bundle.pm
@@ -0,0 +1,22 @@
+package Mojolicious::Plugin::Bundle;
+use strict;
+
+1;
+
+
+__END__
+
+# ABSTRACT: Collection of mojolicious plugins
+
+=head1 SYNOPSIS
+
+#In mojolicious application
+
+$self->plugin('yml_config');
+
+$self->plugin('asset_tag_helper');
+
+$self->plugin('modware');
+
+$self->plugin('bcs');
+
